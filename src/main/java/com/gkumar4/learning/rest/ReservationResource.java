33f7b82ffc3d58a45064ca7039a8cdc42d840a9d
Gaurav Kumar <gkumar4@outlook.com>
2019-01-01 11:01:01 +0530

    Update ReservationResource.java

--- src/main/java/com/gkumar4/learning/rest/ReservationResource.java
+++ src/main/java/com/gkumar4/learning/rest/ReservationResource.java
@@ -17,7 +17,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.time.LocalDate;
 
 @RestController
